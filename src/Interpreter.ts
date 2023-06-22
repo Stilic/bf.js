39b5c31e2c88d6b8fd1ed1b997821de275107c9f
Stilic <lateman-jpeg@outlook.fr>
2023-06-22 22:16:20 +0200

    refactor: parse using an ast

---
+++ src/Interpreter.ts
@@ -0,0 +1,88 @@
+import { Token, TokenType } from "./Parser";
+
+export default class Interpreter {
+  public readonly source: Token[];
+
+  public onInput: () => string;
+
+  private cells: number[];
+  private currentCell: number;
+
+  constructor(source: Token[]) {
+    this.source = source;
+  }
+
+  private prepareCurrentCell() {
+    if (!Object.prototype.hasOwnProperty.call(this.cells, this.currentCell))
+      this.cells[this.currentCell] = 0;
+  }
+
+  private executeCommand(command: Token): string | undefined {
+    let output: string | undefined;
+
+    switch (command.type) {
+      case TokenType.MoveLeft:
+        if (this.currentCell > 0) this.currentCell--;
+        this.prepareCurrentCell();
+        break;
+
+      case TokenType.MoveRight:
+        this.currentCell++;
+        this.prepareCurrentCell();
+        break;
+
+      case TokenType.Increment:
+      case TokenType.Decrement:
+        this.prepareCurrentCell();
+        if (command.type === TokenType.Increment)
+          this.cells[this.currentCell]++;
+        else this.cells[this.currentCell]--;
+        break;
+
+      case TokenType.Output:
+        this.prepareCurrentCell();
+        output = String.fromCharCode(this.cells[this.currentCell]);
+        break;
+
+      case TokenType.Input:
+        if (this.onInput) {
+          const input = this.onInput();
+          if (input.length === 0) throw "Input can't be empty!";
+          else {
+            this.prepareCurrentCell();
+            this.cells[this.currentCell] = input.charCodeAt(0);
+          }
+        }
+        break;
+
+      case TokenType.Loop:
+        if (command.content) {
+          output = "";
+          while (this.cells[this.currentCell] !== 0) {
+            for (let i = 0; i < command.content.length; ++i) {
+              const char = this.executeCommand(command.content[i]);
+              if (char) output += char;
+            }
+          }
+        }
+    }
+
+    return output;
+  }
+
+  public async run(): Promise<string> {
+    let output = "";
+
+    this.cells = [];
+    this.currentCell = 0;
+
+    this.prepareCurrentCell();
+
+    for (let i = 0; i < this.source.length; ++i) {
+      const char = this.executeCommand(this.source[i]);
+      if (char) output += char;
+    }
+
+    return output;
+  }
+}
---
+++ src/Parser.ts
@@ -0,0 +1,59 @@
+export enum TokenType {
+  MoveLeft,
+  MoveRight,
+  Increment,
+  Decrement,
+  Output,
+  Input,
+  Loop,
+}
+
+export type Token = {
+  type: TokenType;
+  content?: Token[];
+};
+
+export default function parse(program: string) {
+  const groups: Token[][] = [[]];
+  let groupIndex = 0;
+
+  for (let i = 0; i < program.length; ++i) {
+    switch (program[i]) {
+      case "<":
+        groups[groupIndex].push({ type: TokenType.MoveLeft });
+        break;
+
+      case ">":
+        groups[groupIndex].push({ type: TokenType.MoveRight });
+        break;
+
+      case "+":
+        groups[groupIndex].push({ type: TokenType.Increment });
+        break;
+
+      case "-":
+        groups[groupIndex].push({ type: TokenType.Decrement });
+        break;
+
+      case ".":
+        groups[groupIndex].push({ type: TokenType.Output });
+        break;
+
+      case ",":
+        groups[groupIndex].push({ type: TokenType.Input });
+        break;
+
+      case "[":
+        const token = { type: TokenType.Loop, content: [] };
+        groups[groupIndex].push(token);
+        groupIndex = groups.push(token.content) - 1;
+        break;
+
+      case "]":
+        groups.pop();
+        groupIndex--;
+    }
+  }
+
+  return groups[0];
+}
